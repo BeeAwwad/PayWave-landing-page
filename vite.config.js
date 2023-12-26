4a8c30af79a3a8d7fc9c327dfdbb39694a25f9fa
BeeAwwad <aosalihu@gmail.com>
2023-12-26 21:12:29 +0100

    add: deploy workflow and third try at deployment :(

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/PayWave-landing-page",
+  base: "/PayWave-landing-page/",
   plugins: [react()],
 })
