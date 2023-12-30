9ace85c52d3ac79d34aa4e2a7382554d077d9238
BeeAwwad <aosalihu@gmail.com>
2023-12-30 20:00:11 +0100

    add: Added a new footer and a new testimonials section :X

--- src/landingPageComponents/Main.jsx
+++ src/landingPageComponents/Main.jsx
@@ -120,7 +120,7 @@ const Main = () => {
       </section>
 
       <section id="features" className="py-12">
-        <div className="container mx-auto">
+        <div className="container mx-auto mb-8">
           <div className="text-[#094067]  mb-8">
             <h2 className="text-3xl font-bold text-center">
               Enjoy these features
@@ -153,20 +153,50 @@ const Main = () => {
             ))}
           </div>
         </div>
+        <div className="flex flex-col md:flex-row md:items-center justify-center p-5">
+          <h2 className="text-center mb-4 md:mb-0 md:mr-5 font-bold text-2xl">
+            Are you ready to PayWave?
+          </h2>
+          <Button className={""}>Get started</Button>
+        </div>
       </section>
 
-      <section id="testimonials" className="bg-gray-200 py-12">
-        <div className="container mx-auto">
-          <h2 className="text-3xl font-bold mb-8">What Users Say</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div>
-              <blockquote className="text-lg">
-                &quot;Using PayWave has made managing my finances a breeze. No
-                more juggling multiple platforms!&quot; - John Doe
-              </blockquote>
-              {/* Include other testimonials */}
-            </div>
-          </div>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+          <figure className="max-w-screen-md mx-auto">
+            <svg
+              className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600"
+              viewBox="0 0 24 27"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+                fill="currentColor"
+              />
+            </svg>
+            <blockquote>
+              <p className="text-2xl font-medium text-gray-900 dark:text-white">
+                `Using PayWave has made managing my finances a breeze. No more
+                juggling multiple platforms!`
+              </p>
+            </blockquote>
+            <figcaption className="flex items-center justify-center mt-6 space-x-3">
+              <img
+                className="w-6 h-6 rounded-full"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
+                alt="profile picture"
+              />
+              <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                <div className="pr-3 font-medium text-gray-900 dark:text-white">
+                  Micheal Gough
+                </div>
+                <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">
+                  CEO at Google
+                </div>
+              </div>
+            </figcaption>
+          </figure>
         </div>
       </section>
 
